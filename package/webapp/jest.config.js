a17f93a8fe4e2a210e530a046e5dc7b21d315601
Michael Koppen <michael.koppen@googlemail.com>
2021-12-16 20:06:29 +0100

    fix: Disallow jest spec files

--- package/webapp/jest.config.js
+++ package/webapp/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  testRegex: `.*(test)\.[jt]sx?$`,
   testEnvironment: 'jsdom',
   moduleDirectories: ['node_modules', 'src'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
